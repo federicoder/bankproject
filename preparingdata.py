92f551d9e4a645e0bb9d48366589794d8f4133e2
Mauro Giordano <mauro.giordano1995@gmail.com>
2022-10-27 17:34:47 +0200

    Code Optimization on preparingdata

--- preparingdata.py
+++ preparingdata.py
@@ -3,42 +3,53 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sys
+pd.options.display.max_rows = None
+pd.options.display.max_columns = None
 import tkinter
 import utils_service as us
 from imblearn.under_sampling import NearMiss
-import seaborn as sns
-import sys
 
 sys.stdout = open('./output/report.txt', 'wt')
 sns.set(palette="Set2")
 matplotlib.use('TkAgg')
 
-ds = pd.read_csv("./dataset//Churn_Modelling.csv")
+## Read the data frame
+ds = pd.read_csv("./dataset/Churn_Modelling.csv")
 
 ### DATA ANALISYS
+
+## Show number of rows and columns
 print('number of rows, number of columns ')
 print(ds.shape)
-# checking column list and missing values:
-print('checking column list and missing values:')
+print('--------------------------------------------')
+
+## Check column list and missing values
+print('Check column list and missing values:')
 print(ds.isnull().sum())
+print('--------------------------------------------')
+
+## Get unique count for each variable
 print('Get unique count for each variable:')
 print(ds.nunique())
+print('--------------------------------------------')
+
+## Get duplicated data
 print('Get duplicated data:')
 print(ds[ds.duplicated()])
-print(ds.corr())
+print('--------------------------------------------')
 
-# casting to int64 float attributes:
-ds['EstimatedSalary'] = ds['EstimatedSalary'].astype(np.int64)
-ds['Balance'] = ds['Balance'].astype(np.int64)
-
-# remove unrelevant features:
-ds.drop(["RowNumber", "CustomerId", "Surname"], axis=1, inplace=True)
-# first five row of the dataset
-print(ds.head())
+## Check variable data types
+print('Check variable data types:')
 print(ds.dtypes)
+print('--------------------------------------------')
 
-# Here our main interest is to get an understanding as to how the given attributes relate too the 'Exit' status.
+## Show first five rows of the dataset
+print('First five rows of the dataset:')
+print(ds.head())
+print('--------------------------------------------')
 
+## Proportion of customer churned and retained
 labels = 'Exited', 'Retained'
 sizes = [ds.Exited[ds['Exited'] == 1].count(), ds.Exited[ds['Exited'] == 0].count()]
 explode = (0, 0.1)
@@ -50,16 +61,66 @@ plt.title("Proportion of customer churned and retained", size=20)
 plt.savefig("./output/proportions_of_customer_churned.png")
 plt.show()
 
-# We first review the 'Status' relation with categorical variables
+## Relations based on the categorical variables
 fig, axarr = plt.subplots(2, 2, figsize=(20, 12))
 sns.countplot(x='Geography', hue='Exited', data=ds, ax=axarr[0][0])
 sns.countplot(x='Gender', hue='Exited', data=ds, ax=axarr[0][1])
 sns.countplot(x='HasCrCard', hue='Exited', data=ds, ax=axarr[1][0])
 sns.countplot(x='IsActiveMember', hue='Exited', data=ds, ax=axarr[1][1])
-plt.savefig("./output/proportions_RelevantFeatures_Exited.png")
-### FEATURE ENGINEERING :
+plt.savefig("./output/proportions_RelevantCategoricalFeatures_Exited.png")
 
-# detecting outliers:
+## Relations based on the continuous variables
+fig, axarr = plt.subplots(3, 2, figsize=(20, 12))
+sns.boxplot(y='CreditScore',x = 'Exited', hue = 'Exited',data = ds, ax=axarr[0][0])
+sns.boxplot(y='Age',x = 'Exited', hue = 'Exited',data = ds , ax=axarr[0][1])
+sns.boxplot(y='Tenure',x = 'Exited', hue = 'Exited',data = ds, ax=axarr[1][0])
+sns.boxplot(y='Balance',x = 'Exited', hue = 'Exited',data = ds, ax=axarr[1][1])
+sns.boxplot(y='NumOfProducts',x = 'Exited', hue = 'Exited',data = ds, ax=axarr[2][0])
+sns.boxplot(y='EstimatedSalary',x = 'Exited', hue = 'Exited',data = ds, ax=axarr[2][1])
+plt.savefig("./output/proportions_RelevantContinuousFeatures_Exited.png")
+
+## Checking correlation:
+plt.subplots(figsize=(11, 8))
+sns.heatmap(ds.corr(), annot=True, cmap="RdYlBu")
+plt.savefig("./output/correlation.png")
+plt.show()
+
+### PRE-PROCESSING
+
+## Remove unrelevant features:
+ds.drop(["RowNumber", "CustomerId", "Surname"], axis=1, inplace=True)
+
+## Casting to int64 float attributes:
+ds['EstimatedSalary'] = ds['EstimatedSalary'].astype(np.int64)
+ds['Balance'] = ds['Balance'].astype(np.int64)
+
+## Split Dataset in Train set and Test set:
+ds_train = ds.sample(frac=0.8, random_state=200)
+ds_test = ds.drop(ds_train.index)
+print('Total number of rows for the Training set and the Test set:')
+print(len(ds_train))
+print(len(ds_test))
+print('--------------------------------------------')
+
+
+## Add new feature:
+# Balance Salary Ratio:
+ds_train['BalanceSalaryRatio'] = ds_train.Balance / ds_train.EstimatedSalary
+sns.boxplot(y='BalanceSalaryRatio', x='Exited', hue='Exited', data=ds_train)
+plt.ylim(-1, 5)
+# Tenure By Age:
+ds_train['TenureByAge'] = ds_train.Tenure / (ds_train.Age)
+sns.boxplot(y='TenureByAge', x='Exited', hue='Exited', data=ds_train)
+plt.ylim(-1, 1)
+plt.savefig("./output/ds_with_new_feature_TenureByAge.png")
+plt.show()
+# Credit Score Given Age:
+ds_train['CreditScoreGivenAge'] = ds_train.CreditScore / (ds_train.Age)
+print('First five rows of the Training Set with the new Features:')
+print(ds_train.head())
+print('--------------------------------------------')
+
+## Detecting outliers:
 print('detecting outliers:\n')
 plt.figure(figsize=(12, 12))
 bplot = ds.boxplot(patch_artist=True)
@@ -67,57 +128,29 @@ plt.xticks(rotation=90)
 plt.savefig("./output/outliers.png")
 plt.show()
 
-# r removing outlier from estimatedSalary:
-# Split Train, test data:
-ds_train = ds.sample(frac=0.8, random_state=200)
-ds_test = ds.drop(ds_train.index)
-print(len(ds_train))
-print(len(ds_test))
+## Removing outliers:
+#ds_train['EstimatedSalary'] = us.remove_outliers(ds, 'EstimatedSalary')
+#sns.boxenplot(ds['EstimatedSalary'])
+#plt.ylabel('distribution')
+#plt.savefig("./output/removing_outliers_estimated_salary.png")
+#plt.show()
 
-# add new feature more relevant:
-ds_train['BalanceSalaryRatio'] = ds_train.Balance / ds_train.EstimatedSalary
-sns.boxplot(y='BalanceSalaryRatio', x='Exited', hue='Exited', data=ds_train)
-plt.ylim(-1, 5)
-
-# Given that tenure is a 'function' of age, we introduce a variable aiming to standardize tenure over age:
-ds_train['TenureByAge'] = ds_train.Tenure / (ds_train.Age)
-sns.boxplot(y='TenureByAge', x='Exited', hue='Exited', data=ds_train)
-plt.ylim(-1, 1)
-plt.savefig("./output/ds_with_new_feature_TenureByAge.png")
-plt.show()
-
-# Lastly we introduce a variable to capture credit score given age to take into account credit behaviour visavis adult life
-ds_train['CreditScoreGivenAge'] = ds_train.CreditScore / (ds_train.Age)
-
-print(ds_train)
-
-# removing outliers:
-ds_train['EstimatedSalary'] = us.remove_outliers(ds, 'EstimatedSalary')
-sns.boxenplot(ds['EstimatedSalary'])
-plt.ylabel('distribution')
-plt.savefig("./output/removing_outliers_estimated_salary.png")
-plt.show()
-
-# checking correlation:
-plt.subplots(figsize=(11, 8))
-sns.heatmap(ds.corr(), annot=True, cmap="RdYlBu")
-plt.savefig("./output/correlation.png")
-plt.show()
-
-# Arrange columns by data type for easier manipulation
-continuous_vars = ['CreditScore', 'Age', 'Tenure', 'Balance', 'NumOfProducts', 'EstimatedSalary', 'BalanceSalaryRatio',
-                   'TenureByAge', 'CreditScoreGivenAge']
+## Reorder the columns
+continuous_vars = ['CreditScore', 'Age', 'Tenure', 'Balance', 'NumOfProducts', 'EstimatedSalary', 'BalanceSalaryRatio', 'TenureByAge', 'CreditScoreGivenAge']
 cat_vars = ['HasCrCard', 'IsActiveMember', 'Geography', 'Gender']
 ds_train = ds_train[['Exited'] + continuous_vars + cat_vars]
-ds_train.head()
+print('First five rows of the Reordered Training Set:')
+print(ds_train.head())
+print('--------------------------------------------')
 
-'''For the one hot variables, we change 0 to -1 so that the models can capture a negative relation 
-where the attribute in inapplicable instead of 0'''
+## Change the 0 in categorical variables to -1
 ds_train.loc[ds_train.HasCrCard == 0, 'HasCrCard'] = -1
 ds_train.loc[ds_train.IsActiveMember == 0, 'IsActiveMember'] = -1
-ds_train.head()
+print('First five rows of the Training Set after changing the HasCrCard and IsActiveMember values from 0 to -1:')
+print(ds_train.head())
+print('--------------------------------------------')
 
-# Converting values of geography and gender from string to float (to fit the model next time)
+## One hot encode the categorical variables
 lst = ['Geography', 'Gender']
 remove = list()
 for i in lst:
@@ -126,12 +159,15 @@ for i in lst:
             ds_train[i + '_' + j] = np.where(ds_train[i] == j, 1, -1)
         remove.append(i)
 ds_train = ds_train.drop(remove, axis=1)
-ds_train.head()
+print('First five rows of the Training Set after One Hot Encoding:')
+print(ds_train.head())
+print('--------------------------------------------')
 
-# min - max normalization:
-# minMax scaling the continuous variables
+## Min-Max Normalization (min-max scaling the continuous variables):
 print("Min-Max normalization:\n")
 minVec = ds_train[continuous_vars].min().copy()
 maxVec = ds_train[continuous_vars].max().copy()
 ds_train[continuous_vars] = (ds_train[continuous_vars] - minVec) / (maxVec - minVec)
+print('First five rows of the Training Set after min-max normalization:')
 print(ds_train.head())
+print('--------------------------------------------')
